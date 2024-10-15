c657490611ac354a2af48598dfc2c477242c0989
zelalemzola <tzelalemtesfaye@gmail.com>
2024-10-15 17:23:03 +0300

    done

--- src/app/workers/page.jsx
+++ src/app/workers/page.jsx
@@ -124,7 +124,7 @@ const Workers = () => {
     <div className="w-full mx-auto mt-[20px] flex flex-col ">
       <div className="w-full flex flex-wrap items-center justify-between fixed top-0 z-30 bg-white border-b rounded-b-2xl px-[4%] md:px-[10%] py-3 shadow-md">
       <Link href='/'><ArrowLeft fontWeight='bold' size={33} className="text-white bg-primary font-bold p-1 rounded-full md:scale-200 cursor-pointer" /></Link>
-        <p className="text-primary text-lg md:text-2xl font-bold flex items-center gap-6">Available Lawyers</p>
+        <p className="text-primary text-lg md:text-2xl font-bold flex items-center gap-6">Available Experts</p>
         <Dialog className="flex-end">
           <DialogTrigger>
             <Button className="flex items-center gap-3 bg-primary hover:bg-primary">Filter <FilterIcon className='hidden md:block'/></Button>
@@ -196,7 +196,7 @@ const Workers = () => {
         </Dialog>
       </div>
   
-      {(!maidsData || !categoriesData)?<div className='pt-[40%] text-xl text-primary font-bold mx-auto'>Loading Lawyer...</div>
+      {(!maidsData || !categoriesData)?<div className='pt-[40%] text-xl text-primary font-bold mx-auto'>Loading Experts...</div>
       :
             <div className="flex flex-wrap items-center justify-center gap-x-2 md:gap-x-8 gap-y-20 pt-[20%] md:pt-[15%] z-0 ">
         {filteredMaids.map((maid) => (
